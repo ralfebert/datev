24adc68cfc7eb19da3ec695fb0b37e8a8208a1e3
Ralf Ebert <info@ralfebert.de>
2016-09-02 20:12:27 +0200

    account/contact-export entfernt

--- lib/datev/export/account_export.rb
+++
@@ -1,6 +0,0 @@
-module Datev
-  class AccountExport < Export
-    self.header_class = AccountHeader
-    self.row_class = Account
-  end
-end
--- lib/datev/export/contact_export.rb
+++
@@ -1,6 +0,0 @@
-module Datev
-  class ContactExport < Export
-    self.header_class = ContactHeader
-    self.row_class = Contact
-  end
-end
